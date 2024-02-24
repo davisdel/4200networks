d0e5c87fd3622fab6ecc16f2317d19f9f9da3ae0
davisdel <davisdeloach@gmail.com>
2024-02-23 20:50:18 -0600

    working, needs log file added

--- client.py
+++ client.py
@@ -1,15 +1,37 @@
-import getopt, sys
+import getopt, sys, socket
 
-print('Making connection.')
 argumentList = sys.argv[1:]
 options = "s:p:l:"
 try:
     arguments, values = getopt.getopt(argumentList, options)
     for currentArgument, currentValue in arguments:
         if currentArgument in ("-s"):
-            print(currentValue)
+            ipAddr = currentValue
         elif currentArgument in ("-p"):
-            print(currentValue)
+            port = int(currentValue)
+        elif currentArgument in ("-l"):
+            logFile = currentValue
 except getopt.error as err:
     print(str(err))
-print('Done.')
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# check for errors
+
+serverAddr = (ipAddr, port)
+
+# check for errors
+
+message = input('Please enter your message:  ')
+
+client_socket.connect(serverAddr)
+
+client_socket.sendall(message.encode('utf-8'))
+
+recieved = client_socket.recv(1024)
+
+print(recieved.decode('utf-8'))
+
+# log the returned message
+
+client_socket.close()
