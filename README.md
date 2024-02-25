968482344562c83bf3bdd50f51d2daef0f7beee4
davisdel <davisdeloach@gmail.com>
2024-02-24 19:31:40 -0600

    Fully functional, need to finish up TCPDUMP capture

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# 4200networks
+To run program do:
+
+    python client.py -s 34.118.242.76 -p 6543 -l LOGFILE
+
+Result will be stored in LOGFILE.txt if "Network" is sent to the server
--- client.py
+++ client.py
@@ -1,7 +1,12 @@
 import getopt, sys, socket
 
+# read in user inputs
 argumentList = sys.argv[1:]
+
+# create options to check for specific arguments
 options = "s:p:l:"
+
+# store each of the inputs into variables and check for any errors
 try:
     arguments, values = getopt.getopt(argumentList, options)
     for currentArgument, currentValue in arguments:
@@ -14,27 +19,30 @@ try:
 except getopt.error as err:
     print(str(err))
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# create a socket and check for errors
+try:
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverAddr = (ipAddr, port)
+# initialize connection
+    client_socket.connect(serverAddr)
+# take in user input from the command line
+    message = input('Please enter your message:  ')
+# send user input to the server
+    client_socket.sendall(message.encode('utf-8'))
+    client_socket.settimeout(5)
+    recieved = client_socket.recv(1024)
 
-# check for errors
-
-serverAddr = (ipAddr, port)
-
-# check for errors
-
-message = input('Please enter your message:  ')
-
-client_socket.connect(serverAddr)
-
-client_socket.sendall(message.encode('utf-8'))
-
-recieved = client_socket.recv(1024)
-recieved = (recieved.decode('utf-8'))
-
+    if recieved:
+        recieved = (recieved.decode('utf-8'))
 # log the returned message
+        file = open("{0}.txt".format(logFile), "a")
+        file.write("{0}\n".format(recieved))
+        file.close()
 
-f = open("{0}.txt".format(logFile), "a")
-f.write("{0}\n".format(recieved))
-f.close()
+    client_socket.close()
 
-client_socket.close()
+except socket.timeout:
+    print("No server response")    
+
+except socket.error as err:
+    print(str(err))
